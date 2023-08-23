0946232b643a6a9e39946ec1a38bd71a93c8d566
soronbekov-a-auca-2021 <soronbekov_a@auca.kg>
2023-08-24 05:27:24 +0600

    Changes

--- Neobis_iOS_Calculating/CalculatorViewController.swift
+++ Neobis_iOS_Calculating/CalculatorViewController.swift
@@ -4,39 +4,55 @@
 
 import UIKit
 
- class ViewController: UIViewController {
-
-     let mainView = CalculatorView()
-     let calculator = CalculatorModel(operand1: 0, operand2: 0, currentOperation: .add)
-
-     override var preferredStatusBarStyle: UIStatusBarStyle {
-         .lightContent
-     }
-
-     override func viewDidLoad() {
-         super.viewDidLoad()
-
-         addToView()
-         constraints()
-     }
-
-     func addToView() {
-         view.addSubview(mainView)
-
-     }
-
-     func constraints() {
-         mainView.translatesAutoresizingMaskIntoConstraints = false
-
-         NSLayoutConstraint.activate([
-             mainView.topAnchor.constraint(equalTo: view.topAnchor),
-             mainView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-             mainView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-             mainView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-
-         ])
-
-     }
- }
+class ViewController: UIViewController {
+    
+    let mainView = CalculatorView()
+    var answer: Double = 0
+    var calculator = CalculatorModel(operand1: 0, operand2: 0, currentOperation: .add)
+    
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addToView()
+        addConstraints()
+    }
+    
+    func addToView() {
+        view.addSubview(mainView)
+        
+    }
+    
+    func calculate() {
+        mainView.operationPressed = {(operand1, currentOperation) in
+            switch currentOperation{
+            case 11:
+                if self.answer == 0 {
+                    self.answer = Double(operand1)
+                    self.calculator.currentOperation = .add
+                }
+            default:
+                break
+            }
+        }
+    }
+    
+    func addConstraints() {
+        mainView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mainView.topAnchor.constraint(equalTo: view.topAnchor),
+            mainView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mainView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mainView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+        ])
+        
+    }
+}
 
 
